ef439575b267e8630bb7f5114baf66ee6cc38fd0
MCamachoGarcia <mcamachogarcia@frba.utn.edu.ar>
2026-01-05 09:07:26 -0300

    Logica de Intefaz con Swing - Ejemplo Agenda electronica

---
+++ AgendaElectronica/src/agendaelectronica/AgendaElectronica.java
@@ -0,0 +1,14 @@
+package agendaelectronica;
+
+import agendaelectronica.igu.Principal;
+
+public class AgendaElectronica {
+
+    public static void main(String[] args) {
+        
+        Principal pantalla = new Principal();
+        pantalla.setVisible(true);
+        pantalla.setLocationRelativeTo(null);
+    }
+    
+}
