98fcbd8f54072da0460957d9cca8f128627ec06e
MCamachoGarcia <mcamachogarcia@frba.utn.edu.ar>
2026-01-06 17:00:17 -0300

    Finalizado Parte 1 - Ejemplo Teatro y asientos

--- ReservaAsientos/src/reservaasientos/ReservaAsientos.java
+++ ReservaAsientos/src/reservaasientos/ReservaAsientos.java
@@ -6,48 +6,68 @@ public class ReservaAsientos {
 
     public static void main(String[] args) {
         
-        
-        //ctrl + espacio: suggestions
-        
+        //Declaracion de variables
         char asientos[][] = new char[10][10]; //Matriz de 10 filas y 10 asientos c/u
         boolean bandera = false;
         Scanner teclado = new Scanner(System.in);
-        int fila, asiento;
+        int fila = 0, asiento = 0; //Deben ser inicializados debido a las validaciones
         String respuesta;
+        String verMapa;
         
         //Recorro y cargo la matriz
         for(int f = 0; f<10; f++){
             for(int c = 0; c<10; c++){
                 asientos[f][c] = 'L';
-                System.out.print(asientos[f][c] + " ");
+                //System.out.print(asientos[f][c] + " ");
             }
-            System.out.println();
+            //System.out.println();
         }
         
+        //Bienvenida al sistema
         System.out.println("---------Bienvenido al Sistema de Reservas---------");
         
+        //Reserva de asientos
         while(bandera == false){
-            System.out.println("Ingrese Fila y Asiento a reservar");
             
-            System.out.print("Fila (entre 0 y 9): ");
-            fila = teclado.nextInt();
+            //Visualizacion del mapa
+            System.out.println("Desea usted ver los asientos disponibles? S: si. Cualquier caracter: no");
+            verMapa = teclado.next();
             
-            System.out.print("Asiento (entre 0 y 9): ");
-            asiento = teclado.nextInt();
+            if(verMapa.equalsIgnoreCase("S")){
+                mostrarMapa(asientos);
+            }
+            
+            
+            //Reserva
+            System.out.println("\nIngrese Fila y Asiento a reservar");
+            
+            boolean valorIngresadoOk = false; //Importante: Debe estar dentro del while para reiniciarse en cada iteracion su valor a false
+            while(valorIngresadoOk == false){
+            
+                System.out.print("Fila (entre 0 y 9): ");
+                fila = teclado.nextInt();
+
+                System.out.print("Asiento (entre 0 y 9): ");
+                asiento = teclado.nextInt();
+                
+                if(fila>=0 && fila<=9){
+                    if(asiento>=0 && asiento<=9){
+                        valorIngresadoOk = true;
+                    }
+                    else{
+                        System.out.println("El numero de asiento no es valido.");
+                    }
+                }
+                else{
+                    System.out.println("El numero de fila no es valido");
+                }
+            }
             
             //char no es un objeto. String si lo es, y ahi si podria usar asientos[fila][asiento].equals("L")
             //char es un tipo de dato primitivo
             if(asientos[fila][asiento] == 'L'){
                 asientos[fila][asiento] = 'X';
                 System.out.println("El asiento fue reservado correctamente");
-                
-                //Muestro toda la matriz
-                for(int f = 0; f<10; f++){
-                    for(int c = 0; c<10; c++){
-                        System.out.print(asientos[f][c] + " ");
-                    }
-                    System.out.println();
-                }
             }
             else{
                 System.out.println("El asiento ya se encuentra ocupado");
@@ -64,5 +84,15 @@ public class ReservaAsientos {
             
         }
     }
-    
+ 
+    //Static hace el metodo utilizable para toda la clase
+    //Concepto para usar las variables locales afuera del main las paso como parametros al metodo.
+    static void mostrarMapa(char asientos[][]){
+        for(int f = 0; f<10; f++){
+            for(int c = 0; c<10; c++){
+                System.out.print("[" + asientos[f][c] + "]");
+            }
+        System.out.println();
+        }
+    }
 }
